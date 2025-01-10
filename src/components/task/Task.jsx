98d81fbdc9bac6ed2cad493d240a008e6dec0fad
KornelijusKat <katinaskornelijus@gmail.com>
2025-01-10 20:22:37 +0200

    updated task

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,14 +1,24 @@
+import { Link, useParams } from "react-router-dom";
 
 
 
 const Task = (props) =>{
+    const { id }= useParams()
+    console.log(props.id)
     return (
+        
         <div className="task m-1">
             <p><strong>Name:</strong> {props.name}</p>
             <p><strong>description:</strong> {props.description}</p>
             <p><strong>Priority:</strong> {props.priority}</p>
             <p><strong>Due Date:</strong> {props.to}</p>
             <p><strong>------------------------</strong></p>
+            <Link
+                to={`/project/${id}/updatetask/${props.id}`}
+                className="btn btn-sm btn-outline-primary border-0 me-2"
+            >
+                Update Task
+            </Link>
         </div>
     );
 };
