6ac226d6d43d6a2f61a62d77fe1a737db90dbbbe
sarunasbit <sarunas.bit@gmail.com>
2025-01-12 22:05:16 +0200

    auth rework

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,26 +12,7 @@ const initialState = {
 }
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState)
-    const firebaseAuthErrors = {
-        // Common errors for both login and registration
-        "auth/invalid-email": "Invalid email format.",
-        "auth/user-disabled": "This account has been disabled.",
-        "auth/user-not-found": "No account found with this email.",
-        "auth/wrong-password": "Incorrect password.",
-        "auth/email-already-in-use": "This email is already registered.",
-        "auth/weak-password": "Password must be at least 6 characters.",
-        "auth/operation-not-allowed": "This operation is not allowed.",
-      
-        // Registration-specific errors
-        "auth/missing-email": "Email is required.",
-        "auth/missing-password": "Password is required.",
-      
-        // Login-specific errors
-        "auth/invalid-credential": "Invalid Email or Password.",
-        "auth/too-many-requests": "Too many attempts. Please try again later.",
-        "auth/network-request-failed": "Network error. Check your connection.",
-      };
-      
+
     const [user, loading] = useAuthState(auth)
 
     useEffect(() => {
@@ -54,7 +35,7 @@ const AppProvider = ({children}) => {
         fetchData();
     }, [user, loading]);
     return (
-        <AppContext.Provider value={{state, dispatch, firebaseAuthErrors}}>
+        <AppContext.Provider value={{state, dispatch}}>
             {children}
         </AppContext.Provider>
     )
---
+++ src/helpers/authError.js
@@ -0,0 +1,19 @@
+export const firebaseAuthErrors = {
+    // Common errors for both login and registration
+    "auth/invalid-email": "Invalid email format.",
+    "auth/user-disabled": "This account has been disabled.",
+    "auth/user-not-found": "No account found with this email.",
+    "auth/wrong-password": "Incorrect password.",
+    "auth/email-already-in-use": "This email is already registered.",
+    "auth/weak-password": "Password must be at least 6 characters.",
+    "auth/operation-not-allowed": "This operation is not allowed.",
+  
+    // Registration-specific errors
+    "auth/missing-email": "Email is required.",
+    "auth/missing-password": "Password is required.",
+  
+    // Login-specific errors
+    "auth/invalid-credential": "Invalid Email or Password.",
+    "auth/too-many-requests": "Too many attempts. Please try again later.",
+    "auth/network-request-failed": "Network error. Check your connection.",
+  };
