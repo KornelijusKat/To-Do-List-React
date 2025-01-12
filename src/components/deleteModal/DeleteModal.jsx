e5721f14759ab94e777bfcc6a5bf2e53902bc9a3
KornelijusKat <katinaskornelijus@gmail.com>
2025-01-12 20:38:13 +0200

    fixed redirection for user

--- src/components/deleteModal/DeleteModal.jsx
+++ src/components/deleteModal/DeleteModal.jsx
@@ -6,20 +6,36 @@ const DeleteModal = ({deleteRecord}) => {
     const closeModal = () => {
         dispatch({ type: "TOGGLE_MODAL", payload: { showModal: false } });
     };
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            // Check if the click is outside `.modal-content`
+            if (!e.target.closest(".modal-content")) {
+                closeModal();
+            }
+        };
+
+        // Attach the event listener to the document
+        document.addEventListener("mousedown", handleClickOutside);
+
+        // Cleanup the event listener on component unmount
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
     return(
-        <div className="modal-overlay">
+        <div className="modal-overlay ">
             <div className="row modal-content">
                 <div className="col modal-buttons">
                     <h4>Are you sure you want to delete this task?</h4>
                     <div className="modal-buttons">
                             <button 
-                                className="btn btn-danger"
+                                className="btn btn-danger me-2"
                                 onClick={deleteRecord}
                             >
                                 Yes, Delete
                             </button>
                             <button 
-                                className="btn btn-secondary"
+                                className="btn btn-secondary me-2"
                                 onClick={closeModal}
                             >
                                 Cancel
